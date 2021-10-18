ac955c441cac9e77abee72c342645d8ea0e32aff
shivam-op <84925754+shivam-op@users.noreply.github.com>
2021-10-18 09:47:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,10 @@ SESSION_NAME = getenv("SESSION_NAME", "session")
 BOT_TOKEN = getenv("BOT_TOKEN")
 BOT_NAME = getenv("BOT_NAME", "Camila Music")
 BOT_USERNAME = getenv("BOT_NAME", "TotoMusicBot")
-
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "ELECTROBOT_SUPPORT")
+UPDATES_CHANNEL = getenv("BOT_NAME", "electro_updates")
+BROADCAST_AS_COPY = getenv("BROADCAST_AS_COPY", "False")
+LOG_CHANNEL = getenv("LOG_CHANNEL", "-1001193972853")
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
 DATABASE_URL = os.environ.get("DATABASE_URL") # fill with your mongodb url
