08063e5ac10975c5853d63f6128160f7f8a33b94
ZEYROX-XY <92505709+ZEYROX-XY@users.noreply.github.com>
2021-10-18 06:32:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ BOT_NAME = getenv("BOT_NAME", "Camila Music")
 
 API_ID = int(getenv("API_ID"))
 API_HASH = getenv("API_HASH")
-
+DATABASE_URL = os.environ.get("DATABASE_URL")  # fill with your mongodb url
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "7"))
 
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ !").split())
